3f4a005f87e87f10891be05193584425a1cc0fb3
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-11-06 01:03:53 -0700

    js

---
+++ css/responsive.css/js/resp.js
@@ -0,0 +1,11 @@
+burger = document.querySelector(".burger");
+navbar = document.querySelector("navbar");
+navList = document.querySelector(".nav-List");
+rightNav = document.querySelector(".rightNav");
+
+burger.addEventlistner("click", () => {
+  rightNav.classList.toggle("v-class-resp");
+  nav-navList.classList.toggle("v-class-resp");
+  navbar.classList.toggle('h-nav-resp');
+
+});
